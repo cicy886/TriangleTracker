6d1da88d44d2d024d6d338aef1ec8102b9c83a45
Sisi Vieira <cicy886@gmail.com>
2021-08-31 19:06:48 -0700

    define equilateral. tests failed and passed

--- TriangleTracker.Tests/ModelTests/IsTriangleTest.cs
+++ TriangleTracker.Tests/ModelTests/IsTriangleTest.cs
@@ -8,8 +8,19 @@ namespace TriangleTracker.Tests
   {
     [TestMethod]
     public void IsValidTriangle_CheckIfValidTriangle_True(){
-      IsTriangle testTriangle = new IsTriangle();
+      Triangle testTriangle = new Triangle();
       Assert.AreEqual(true, testTriangle.IsValidTriangle(500,2000,2));
+    }
+    [TestMethod]
+    public void IsValidTriangle_CheckIfNotValidTriangle_False(){
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(false, testTriangle.IsValidTriangle(2,2,8));
+    }
+
+    [TestMethod]
+    public void GetTriangleType_GetTypeOfTheTriangle_Isosceles(){
+      Triangle triangleType = new Triangle();
+      Assert.AreEqual("equilateral", triangleType.GetTriangleType(3,3,3));
     }
   }
 }
