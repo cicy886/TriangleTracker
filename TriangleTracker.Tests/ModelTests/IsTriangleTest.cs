441bd6881f00d16cfb53b83c9dd28292ecc52de5
Sisi Vieira <cicy886@gmail.com>
2021-08-31 19:32:36 -0700

    define isosceles. tests failed and passed

--- TriangleTracker.Tests/ModelTests/IsTriangleTest.cs
+++ TriangleTracker.Tests/ModelTests/IsTriangleTest.cs
@@ -4,23 +4,32 @@ using TriangleTracker;
 namespace TriangleTracker.Tests
 {
   [TestClass]
-  public class IsTriangleTests
+  public class TriangleTests
   {
     [TestMethod]
     public void IsValidTriangle_CheckIfValidTriangle_True(){
       Triangle testTriangle = new Triangle();
       Assert.AreEqual(true, testTriangle.IsValidTriangle(500,2000,2));
     }
+    // [TestMethod]
+    // public void IsValidTriangle_CheckIfNotValidTriangle_False(){
+    //   Triangle testTriangle = new Triangle();
+    //   Assert.AreEqual(false, testTriangle.IsValidTriangle(2,2,8));
+    // }
+
     [TestMethod]
-    public void IsValidTriangle_CheckIfNotValidTriangle_False(){
-      Triangle testTriangle = new Triangle();
-      Assert.AreEqual(false, testTriangle.IsValidTriangle(2,2,8));
+    public void GetTriangleType_GetTypeOfTheTriangle_EquilateralAndIsosceles(){
+      Triangle triangleType = new Triangle();
+      Assert.AreEqual("equilateral and isosceles", triangleType.GetTriangleType(3,3,3));
     }
 
     [TestMethod]
     public void GetTriangleType_GetTypeOfTheTriangle_Isosceles(){
       Triangle triangleType = new Triangle();
-      Assert.AreEqual("equilateral", triangleType.GetTriangleType(3,3,3));
+      Assert.AreEqual("isosceles", triangleType.GetTriangleType(1,2,3));
     }
   }
+
+
+
 }
